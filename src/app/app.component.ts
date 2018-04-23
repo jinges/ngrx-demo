adceefb7d90f21eb6a60efaf93a794c2171a09fb
jinges <jinges2012@gmail.com>
2018-04-23 14:40:32 +0800

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,41 @@ export class AppComponent {
   public firstNum: number;
   public secdNum: number;
   public result: number;
+  public math = '+'
 
   changeFirstNum(val): void {
     if (!isNaN(val)) {
       this.firstNum = parseFloat(val);
     }
     if (!isNaN(this.secdNum)) {
-      this.result = this.firstNum + this.secdNum;
+      this.result = this.compute(this.math, this.firstNum , this.secdNum);
     }
   }
+
   changeSecdNum(val): void {
     if (!isNaN(val)) {
       this.secdNum = parseFloat(val);
     }
     if (!isNaN(this.firstNum)) {
-      this.result = this.firstNum + this.secdNum;
+      this.result = this.compute(this.math, this.firstNum, this.secdNum);
+    }
+  }
+
+  changeMathFun(val){
+    this.math = val;
+    this.result = this.compute(this.math, this.firstNum, this.secdNum);
+  }
+
+  compute(math, first, secd){
+    switch (math) {
+      case '+':
+        return first + secd;
+      case '-':
+        return first - secd;
+      case 'x':
+        return first * secd;
+      case '/':
+        return first / secd;
     }
   }
 }
